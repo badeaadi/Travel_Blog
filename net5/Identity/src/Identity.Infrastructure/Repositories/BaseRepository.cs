1e1b53f9ca7b1753e7a58044220544b9988ce820
vlad_stoleru <vlad_stoleru@yahoo.com>
2022-11-19 15:32:29 +0200

    feat(Identity) - Initial commit

---
+++ net5/Identity/src/Identity.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using Identity.Infrastructure.Persistence.Contexts;
+using Identity.Infrastructure.Persistence.Contexts.Provider;
+
+namespace Identity.Infrastructure.Repositories
+{
+    public class BaseRepository
+    {
+        protected AppDbContext Context { get; }
+        
+        protected BaseRepository(IDbContextProvider dbContextProvider)
+        {
+            Context = dbContextProvider.GetOrCreateContext();
+        }
+    }
+}
