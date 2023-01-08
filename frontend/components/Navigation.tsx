003da8e6ad3f4c19b3422adcd59f59feac5c419d
Adam Alexandru <alexandruvlad.adam@gmail.com>
2023-01-08 18:56:45 +0200

    Added blog card and blog feed

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-/* eslint-disable @next/next/no-img-element */
 export default function Navigation() {
     return (
         <>
---
+++ frontend/pages/feed/index.tsx
@@ -0,0 +1,39 @@
+import BlogCard from "../../components/BlogCard";
+
+const mockedBlogs = [
+    {
+        title: "Lorem ipsum",
+        summary: "Lorem ipsum 2",
+        id: '1',
+        photo: "https://picsum.photos"
+    },
+    {
+        title: "Lorem ipsum 414114",
+        summary: "Lorem ipsum 3",
+        id: '2',
+        photo: "https://picsum.photos/"
+    },
+    {
+        title: "Lorem ipsum 22",
+        summary: "Lorem ipsum 4",
+        id: '3',
+        photo: "https://picsum.photos/"
+    },
+    {
+        title: "Lorem ipsum 2231311",
+        summary: "Lorem ipsum 433333",
+        id: '4',
+        photo: "https://picsum.photos/"
+    }
+];
+
+export default function Feed() {
+
+    return (
+        <div className="m-4 p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {mockedBlogs.map(blog => {
+                return <BlogCard title={blog.title} summary={blog.summary} id={blog.id} photo={blog.photo} />
+            })}
+        </div>
+    );
+}
